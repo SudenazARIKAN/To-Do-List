beef0c2525e4d03615eff240f845edf04487298f
SudenazARIKAN <148457798+SudenazARIKAN@users.noreply.github.com>
2025-04-21 20:33:34 +1200

    Add files via upload

---
+++ Quasar-To Do List/quasar-project/src/pages/HelpPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet,consectetur adipisemper ipsum,et laoreet sapien mauris vitae arcu.</p>
+    <p>Lorem ipsum dolor sit amet,consectetur adipisemper ipsum,et laoreet sapien mauris vitae arcu.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  
+})
+</script>
